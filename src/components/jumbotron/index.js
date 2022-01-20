394fe860473bea16a5958829f9785b851190a92a
caseycantrell <cantrellcasey@gmail.com>
2022-01-20 15:50:38 -0600

    adds jumbotron component and dummy data

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,14 @@
+import { Inner, Container } from './styles/jumbotron' 
+
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+    return (
+        <Inner direction={direction} >
+            {children}
+        </Inner>
+    )
+};
+
+Jumbotron.Container = function JumbotronContainer({children, ...restProps}) {
+    return <Container { ...restProps}>{children}</Container>
+};
+
