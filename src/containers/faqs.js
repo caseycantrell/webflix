359ceed16cc87fe2cdcd836565e424412f0785f2
caseycantrell <cantrellcasey@gmail.com>
2022-01-21 13:30:14 -0600

    adds styling to opt form

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -15,6 +15,7 @@ export function FaqsContainer() {
             <OptForm>
                 <OptForm.Input placeholder="Email Address"></OptForm.Input>
                 <OptForm.Button>Try It Now</OptForm.Button>
+                <OptForm.Break />
                 <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
             </OptForm> 
         </Accordion>
