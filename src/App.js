29126348ac86f12edf908e5025459dd0a5999573
caseycantrell <cantrellcasey@gmail.com>
2022-01-21 13:59:20 -0600

    adds routes and reconfigs app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import { Home, Browse, Signin, Signup } from './pages'
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
-  )
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
+  );
 }
 
