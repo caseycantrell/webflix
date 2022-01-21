45637bcf7fcc00ebbd6eb821a4f515b8c31bde45
caseycantrell <cantrellcasey@gmail.com>
2022-01-21 14:08:07 -0600

    fixes route config

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, Signin, Signup } from './pages'
 
 export default function App() {
   return (
-    <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/browse" element={<Browse/>} />
+        <Route path="/signin" element={<Signin/>} />
+        <Route path="/signup" element={<Signup/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
