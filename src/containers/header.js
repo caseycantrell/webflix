d7af5f3a055ad13bab376c6e5a9076f34d2120a9
caseycantrell <cantrellcasey@gmail.com>
2022-01-21 14:13:30 -0600

    adds header component

---
+++ src/containers/header.js
@@ -0,0 +1,12 @@
+import { Header } from "../components/accordion/styles/accordion";
+
+export default function HeaderContainer() {
+    return (
+        <Header>
+            <Header.Frame>
+                <Header.Logo />
+                <Header.Button />
+            </Header.Frame>
+        </Header>
+    )
+}
