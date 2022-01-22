40d632f4639e40b7abe4f747397e1b309217cd01
caseycantrell <cantrellcasey@gmail.com>
2022-01-22 13:45:30 -0600

    adds form component to signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
 
 export default function Signin() {
+    
+    const [emailAddress, setEmailAddress] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState("");
+
     return (
     <>
-    <HeaderContainer>Hello from the sign in!</HeaderContainer>
+    <HeaderContainer>
+        <Form>
+            <Form.Title>Sign In</Form.Title>
+        </Form>
+    </HeaderContainer>
     <FooterContainer></FooterContainer>
     </>
     );
