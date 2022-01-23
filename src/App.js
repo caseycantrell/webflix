5e13c5a651fde487bc4470be154e2847fe48f795
caseycantrell <cantrellcasey@gmail.com>
2022-01-22 19:17:40 -0600

    reverts route paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { Home, Browse, SignIn, SignUp } from './pages'
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
@@ -9,22 +9,28 @@ export default function App() {
   const user = {};
 
   return (
-    <Routes>
-     
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
-          <SignIn />
-        </IsUserRedirect>
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
-          <SignUp />
-        </IsUserRedirect>
-        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
-          <Browse />
-        </ProtectedRoute>
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
-          <Home />
-        </IsUserRedirect>
-     
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/browse" element={<Browse />}/>
+        <Route path="/signin" element={<SignIn />}/>
+        <Route path="/signup" element={<SignUp />}/>
+
+          {/* <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+            <SignIn />
+          </IsUserRedirect>
+          <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+            <SignUp />
+          </IsUserRedirect>
+          <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+            <Browse />
+          </ProtectedRoute>
+          <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+            <Home />
+          </IsUserRedirect> */}
+      
+      </Routes>
+    </BrowserRouter>
   );
 }
 
