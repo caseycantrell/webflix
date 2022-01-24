fe005f1b0b58b33b375e06fd3c6dde5afd2a3bfc
caseycantrell <cantrellcasey@gmail.com>
2022-01-24 13:34:10 -0600

    adds auth listener

--- src/App.js
+++ src/App.js
@@ -10,26 +10,26 @@ export default function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/browse" element={<Browse />}/>
-        <Route path="/signin" element={<SignIn />}/>
-        <Route path="/signup" element={<SignUp />}/>
+    <Routes>
+     <Route path="/" element={<Home />}/>
+     <Route path="/browse" element={<Browse />}/>
+     <Route path="/signin" element={<SignIn />}/>
+     <Route path="/signup" element={<SignUp />}/>
 
-          {/* <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
-            <SignIn />
-          </IsUserRedirect>
-          <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
-            <SignUp />
-          </IsUserRedirect>
-          <ProtectedRoute user={user} path={ROUTES.BROWSE}>
-            <Browse />
-          </ProtectedRoute>
-          <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
-            <Home />
-          </IsUserRedirect> */}
-      
-      </Routes>
+        {/* <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home />
+        </IsUserRedirect> */}
+     
+    </Routes>
     </BrowserRouter>
   );
 }
